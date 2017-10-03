2a288447352063a8d036e6e5c58dcb25b627d48b
David Jennings <djennings@esri.com>
2017-10-03 22:27:07 +0000

    Updated RestEndpoint

--- src/main/java/org/jennings/metrics/RestEndpoint.java
+++ src/main/java/org/jennings/metrics/RestEndpoint.java
@@ -18,33 +18,67 @@
  */
 package org.jennings.metrics;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.json.JSONObject;
 
 /**
- * Specify a URL 
- * Send request(s) Get/Post that returns Response
- * 
+ * Specify a URL Send request(s) Get/Post that returns Response
+ *
  * @author djennings
  */
 public class RestEndpoint {
-    
+
     String url;  // scheme://host:port
 
     public RestEndpoint(String url) {
         this.url = url;
     }
-    
-    
-    JSONObject get(String path, HashMap<String,String> params) {
+
+    JSONObject get(String path, HashMap<String, String> params) {
         JSONObject json = new JSONObject();
-        
-        
-        
+        try {
+            String url2 = this.url + path;
+
+            SSLContextBuilder builder = new SSLContextBuilder();
+            builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                    builder.build());
+            CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(
+                    sslsf).build();
+
+            HttpGet request = new HttpGet(url2);
+
+            HttpResponse response = httpclient.execute(request);
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            StringBuilder result = new StringBuilder();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+
+            //System.out.println(result);
+            json = new JSONObject(result.toString());
+
+        } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | IOException e) {
+            System.err.println(e.getMessage());
+        }
+
         return json;
     }
-    
-    
-    
-    
+
 }
